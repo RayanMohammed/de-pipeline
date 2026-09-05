2d6ebec0300452040df76a9928726c3f0e1b398f
rayan m <rayanm@rayans-MacBook-Air.local>
2026-09-05 13:46:53 -0700

    locust swarm addition + testing

---
+++ tests/locustfile.py
@@ -0,0 +1,24 @@
+from locust import HttpUser, task, between
+import random
+
+class ClinicalUser(HttpUser):
+    wait_time = between(0.2, 0.8)
+
+    @task(1)
+    def health_check(self):
+        self.client.get("/api/health")
+
+    @task(4)
+    def view_patients(self):
+        limit = random.choice([10, 20, 50])
+        gender = random.choice(["male", "female"])
+        bmi_cat = random.choice(["Underweight", "Normal", "Overweight", "Obese"])
+
+        query_params = {
+            "limit": limit,
+            "gender": gender,
+            "bmi_category": bmi_cat,
+        }
+
+        self.client.get("/api/patients", params=query_params, name="/api/patients")
+        
